e38363af707676d912c5dafea884189ecf28e1bf
SebastianCarcamo <sebastian.carcamo@utec.edu.pe>
2020-06-27 13:34:57 -0500

    Added timer

--- Secuential_KNN.py
+++ Secuential_KNN.py
@@ -1,7 +1,7 @@
 import face_recognition
 import math
 import pickle
-
+import time
  
 def ED(vector1, vector2):
 	res = 0
@@ -24,13 +24,18 @@ class Secuential_KNN:
 	def __init__(self, result_file):
 		with open(result_file,'rb') as f:
 			self.faces = pickle.load(f)
+			self.faces_partition = self.faces
+
+
+	def resize_partition(self,n):
+		self.faces_partition = self.faces[:n]
 
 	def KNN_Faces_ED(self,image_path,n):
 		vec = read_Face(image_path)
 		if len(vec) == 0:
 			print("The image couldn't be identified")
 		else:
-			ed_order = sorted(self.faces,key=lambda x: ED(vec[0],x[1]))
+			ed_order = sorted(self.faces_partition,key=lambda x: ED(vec[0],x[1]))
 		return [item[0] for item in ed_order[:n]]
 
 	def KNN_Faces_MD(self,image_path,n):
@@ -38,9 +43,10 @@ class Secuential_KNN:
 		if len(vec) == 0:
 			print("The image couldn't be identified")
 		else:
-			md_order = sorted(self.faces,key=lambda x: MD(vec[0],x[1]))
+			md_order = sorted(self.faces_partition,key=lambda x: MD(vec[0],x[1]))
 		return [item[0] for item in md_order[:n]]
 
+# Efficency
 ####################################
 sknn = Secuential_KNN('result.pkl')
 
@@ -50,3 +56,13 @@ print("--------------------------------------------")
 print("MD:")
 print(sknn.KNN_Faces_MD('./unknownFaces/nicole_kidman.jpg',19))
 ###################################
+
+#Times
+###################################
+sizes = [100,200,400,800,1600,3200,6400,12800]
+
+for s in sizes:
+	sknn.resize_partition(s)
+	start_time = time.time()
+	sknn.KNN_Faces_ED('./unknownFaces/nicole_kidman.jpg',19)
+	print("--- %s seconds ---" % (time.time() - start_time))
